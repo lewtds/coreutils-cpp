72710efa869cd378258f25e8dc15ed67e5257039
Trung Ngo <ndtrung4419@gmail.com>
2015-05-26 01:14:57 +0700

    Properly handle EOF with `cat`

--- cat.cpp
+++ cat.cpp
@@ -6,7 +6,13 @@ int main(int argc, char *argv[])
     char buf[512];
     while (!std::cin.eof()) {
         std::cin.read(buf, sizeof(buf));
-        std::cout << buf;
+
+        // Check the status of the last read
+        if (std::cin) {
+            std::cout.write(buf, sizeof(buf));
+        } else {
+            std::cout.write(buf, std::cin.gcount());
+        }
     }
 
     return 0;
--- test/test_cat.cpp
+++ test/test_cat.cpp
@@ -1,6 +1,10 @@
-#include "utils.hpp"
 #include "catch.hpp"
+#include "utils.hpp"
 
 TEST_CASE("Use stdin if no input file", "[cat]") {
     REQUIRE(call("echo a | ./cat", NULL) == "a\n");
+}
+
+TEST_CASE("Empty input gives empty output", "[cat]") {
+    REQUIRE(call("echo -n | ./cat", NULL) == "");
 }
